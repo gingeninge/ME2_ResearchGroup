714b7390f98fe6e88d309fdd99c6755540c582f6
Ten10dai <tendaimutshutshu77@gmail.com>
2025-06-03 10:10:11 +0200

    Economic System

---
+++ Assets/Scripts/EconomicSystem.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[System.Serializable]
+public class ShopItem
+{
+    public string itemName;
+    public int itemPrice;
+    public GameObject prefab;
+    public Transform spawnPoint;
+    public Button buyButton;
+
+}
+public class EconomicSystem : MonoBehaviour
+{
+    public int playerMoney;
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI feedbackText;
+    public ShopItem[] shopItems;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdateCurrency();
+        foreach (ShopItem shopItem in shopItems)
+        {
+            shopItem.buyButton.onClick.AddListener(() => BuyItem(shopItem));
+        }
+    }
+
+    private void BuyItem(ShopItem shopItem)
+    {
+        if (playerMoney >= shopItem.itemPrice)
+        {
+            playerMoney -= shopItem.itemPrice;
+            Instantiate(shopItem.prefab, shopItem.spawnPoint.position,Quaternion.identity);
+            feedbackText.text = $"Purchased {shopItem.itemName}";
+        }
+        else
+        {
+            feedbackText.text = "No money";
+        }
+        UpdateCurrency();
+    }
+
+    private void UpdateCurrency()
+    {
+        moneyText.text = $"Money: {playerMoney}";
+
+    }
+}
