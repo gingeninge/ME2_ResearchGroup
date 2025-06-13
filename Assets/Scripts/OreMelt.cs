d7716490e1879021796deaa525d9de75916eb734
gingeninge <thomassheldon539@gmail.com>
2025-06-13 19:00:16 +0200

    testing

--- Assets/Scripts/OreMelt.cs
+++ Assets/Scripts/OreMelt.cs
@@ -1,16 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Filtering;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class OreMelt : MonoBehaviour
 {
+    public XRSocketInteractor Socket;
     public bool isIron = false;
     public bool isSteel = false;
     public bool isCopper = false;
+    public bool isSword = false;
+    public bool isHilt = false;
     public GameObject IronSword;
     public GameObject SteelSword;
     public GameObject CopperSword;
     public Transform spawn;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -19,60 +24,70 @@ public class OreMelt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoldCheck();
+    }
+    public void MoldCheck() 
+    {
+        IXRSelectInteractable selected = Socket.GetOldestInteractableSelected();
+        if (selected == null) return;
+
+        GameObject mold = selected.transform.gameObject;
+        string tag = mold.tag;
+        switch (tag)
+        {
+            case "SwordMold":
+                isSword = true;
+                break;
+            case "SwordHiltMold":
+                isHilt = true;
+                break;
+            default:
+                Debug.LogWarning("Unknown mold tag: " + tag);
+                return;
+        }
     }
     public void SpawnObject() 
     {
-        if (isSteel)
+        if (isSteel && isSword)
         {
             Instantiate(SteelSword, spawn.position, spawn.rotation);
             isSteel = false;
+            isSword=false;
         }
 
-        if (isCopper) 
+        if (isCopper && isSword) 
         {
             Instantiate(CopperSword, spawn.position, spawn.rotation);
             isCopper = false;
+            isSword=false;
         }
 
-        if (isIron) 
+        if (isIron && isSword) 
         {
             Instantiate(IronSword, spawn.position, spawn.rotation);
             isIron = false;
+            isSword=false;
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("IronOre")|| other.gameObject.CompareTag("CopperOre") || other.gameObject.CompareTag("SteelOre"))
         {
-            
             if (other.gameObject.CompareTag("CopperOre") || other.gameObject.CompareTag("SteelOre"))
             {
-              
-               
                 if (other.gameObject.CompareTag("SteelOre"))
                 {
-                ;
-                  isSteel = true;
-                } 
+                    isSteel = true;
+                }
                 else
                 {
                     isCopper = true;
-                   
-             
-                }
-                
+                }   
             }
             else
             {
                isIron = true;
-                
-              
             }
-           
-
         }
-        
-    }
-    
+    } 
 }
